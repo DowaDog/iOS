65e35de86510864bab5c69da31cea5fd1d1c187f
wookeon <creator_wookeon@naver.com>
2019-01-01 13:15:20 +0900

    adopt ing

---
+++ DowaDog/Sources/ViewControllers/Adopt/AdoptDocumentVC.swift
@@ -0,0 +1,21 @@
+//
+//  AdoptDocumentVC.swift
+//  DowaDog
+//
+//  Created by wookeon on 30/12/2018.
+//  Copyright © 2018 wookeon. All rights reserved.
+//
+
+import UIKit
+
+class AdoptDocumentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setNavigationBarClear()
+        self.setBackBtn()
+    }
+    
+    
+}
