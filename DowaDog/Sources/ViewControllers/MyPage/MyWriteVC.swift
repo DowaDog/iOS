78013df1a78763bf027632a0d0d09e5185bac627
조윤영 <yoon980208@gmail.com>
2019-01-08 03:06:36 +0900

    so tired(#yoon)

--- DowaDog/Sources/ViewControllers/MyPage/MyWriteVC.swift
+++ DowaDog/Sources/ViewControllers/MyPage/MyWriteVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MyWriteVC: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var boardTable: UITableView!
     
     var myWriteList = [MyCommunity<CommunityImgList>]()
     
@@ -19,6 +19,14 @@ class MyWriteVC: UIViewController {
         super.viewDidLoad()
         
         setBackBtn()
+        boardTable.dataSource = self
+        boardTable.delegate = self
+        
+        print("======확인=====")
+        print(myWriteList.count)
+              print("======확인=====")
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,12 +38,14 @@ class MyWriteVC: UIViewController {
         
         
         print("transfer=========")
+        
+        
         MyCommunityService.shared.getMyCommunity() { [weak self]
             (data) in
             guard let `self` = self else {return}
             
             self.myWriteList = data
-            
+           
             print("data ===================")
             print(data)
             print("data ===================")
@@ -44,4 +54,39 @@ class MyWriteVC: UIViewController {
     }
     
     
+}
+
+extension MyWriteVC : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myWriteList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = boardTable.dequeueReusableCell(withIdentifier: "BoardCell") as! MyWriteCell
+        
+        let board = myWriteList[indexPath.row]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+
+        cell.dateLabel.text = dateFormatter.string(from: board.updatedAt ?? Date())
+        cell.titleLabel.text = gsno(board.title)
+        
+        return cell
+    }
+}
+
+extension MyWriteVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let detailVC = storyboard?.instantiateViewController(withIdentifier: "DetailBoardVC") as! DetailBoardVC
+//
+//        let board = boardList[indexPath.row]
+//
+//        detailVC.detailBoardId = board.boardId
+//
+//        navigationController?.pushViewController(detailVC, animated: true)
+    }
 }
