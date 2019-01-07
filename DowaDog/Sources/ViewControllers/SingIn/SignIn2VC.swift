8791d2512b377db3a43dc947eb007f2481511dc8
조윤영 <yoon980208@gmail.com>
2019-01-07 14:22:46 +0900

    login logic(#yoon)

--- DowaDog/Sources/ViewControllers/SingIn/SignIn2VC.swift
+++ DowaDog/Sources/ViewControllers/SingIn/SignIn2VC.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
+
+
 class SignIn2VC: UIViewController {
     
-    var emptyCheck = true
-    var idCheck = true
     
+    var name:String?
+    var birth:String?
+    var email:String?
+    var numb:String?
+    var profileImage: UIImage?
+
+    
+    var emptyCheck = false
+    var idCheck = false
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var nextBtn: UIButton!
     
@@ -25,10 +36,29 @@ class SignIn2VC: UIViewController {
         super.viewDidLoad()
         nextBtn.isEnabled = true
         
+        setTarget()
+        
         self.setBackBtn()
+        
+        initGestureRecognizer()
+     
+    }
+
+    
+    func setTarget(){
+        
         idTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         pwTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         pwCheckTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unregisterForKeyboardNotifications()
     }
     
     @objc func textFieldDidChange(textField: UITextField){
@@ -41,17 +71,14 @@ class SignIn2VC: UIViewController {
                 if pwCheckTextField.text != "" {
                     
                     emptyCheck = true
-                    
-                    if idCheck == true {
                         endCheck()
-                    }
                     
                 }
             }
         }
         
     }
-    
+
     func endCheck (){
         if emptyCheck == true && idCheck == true {
             
@@ -71,14 +98,20 @@ class SignIn2VC: UIViewController {
             
             print("성공")
             
+            if data.data == false{
+                
+                 self.simpleAlert(title: "", message: "아이디 사용 가능하개")
+                self.idCheck = true
+                self.endCheck()
+                
+            }
+            else if data.data == true{
+                
+                self.simpleAlert(title: "", message: "아이디를 입력할 수 없어요! 다시 입력해주개")
+                self.idCheck = false
+            }
         
-            self.idCheck = true
-        }
         
-        
-        
-        if emptyCheck == true {
-            endCheck()
         }
         
     }
@@ -86,9 +119,91 @@ class SignIn2VC: UIViewController {
     
     @IBAction func submitAction(_ sender: UIButton) {
         
-        DuplicateService.shared.signUp(id: "wook1234", password: "1234", name: "이승언", birth: "1994-10-04", phone: "010-1234-1234", email: "test1234@naver.com", profileImgFile: UIImage(named: "xBtn")!) {
+        
+        guard let id = idTextField.text else{return}
+        guard let password = pwTextField.text else{return}
+        guard let name = name else{ return}
+        guard let birth = birth else{return}
+        guard let phone = numb else{return}
+        guard let email = email else{return}
+        guard let profile = profileImage else{return}
+        //이미지 꼴 바꾸기 kingfisher
+
+        
+        if pwCheckTextField.text == pwTextField.text{
+           
+            DuplicateService.shared.signUp(id: id, password: password, name: name, birth: birth, phone: phone, email: email, profileImgFile: profile) {
+                
+                 print("회원가입")
+                
+                if let dvc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC {
+                    
+                    //present를 해줍니다.
+                    self.present(dvc, animated: true)
+
+                }
+            }
+        } else if pwCheckTextField.text != pwTextField.text{
             
-            print("회원가입")
+            simpleAlert(title: "", message: "패스워드가 일치하지 않개!")
         }
+    }
+}
+
+
+extension SignIn2VC: UIGestureRecognizerDelegate {
+    
+    func initGestureRecognizer() {
+        let textFieldTap = UITapGestureRecognizer(target: self, action: #selector(handleTapTextField(_:)))
+        textFieldTap.delegate = self
+        view.addGestureRecognizer(textFieldTap)
+    }
+    
+    
+    @objc func handleTapTextField(_ sender: UITapGestureRecognizer){
+        self.idTextField.resignFirstResponder()
+        self.pwTextField.resignFirstResponder()
+        self.pwCheckTextField.resignFirstResponder()
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if (touch.view?.isDescendant(of: idTextField))! || (touch.view?.isDescendant(of: pwTextField))! || (touch.view?.isDescendant(of: pwCheckTextField))! {
+            
+            return false
+        }
+        return true
+    }
+    
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            self.topConstraint.constant = -5
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            self.topConstraint.constant = 79
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
