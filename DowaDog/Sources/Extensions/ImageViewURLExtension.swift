f7ebc8be12fb874c2b041225e024f03219c78581
wookeon <design_pt@naver.com>
2019-01-09 16:42:38 +0900

    trash

--- DowaDog/Sources/Extensions/ImageViewURLExtension.swift
+++ DowaDog/Sources/Extensions/ImageViewURLExtension.swift
@@ -15,7 +15,7 @@ extension UIImageView {
         let defaultImg = UIImage(named: defaultImgPath)
         //혹시나 오류가 발생한 경우 defualtImg 를 출력
         
-        if let url = urlString {
+        if let url = urlString?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
             if url.isEmpty {                //  만약 이미지가 없는 경우
                 self.image = defaultImg
             } else {                        // URL 에 존재하는 경우
