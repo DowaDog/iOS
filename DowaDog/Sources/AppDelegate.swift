778f94697a1d5dcf7f6cd39f6acb0a2d892b59dd
wookeon <design_pt@naver.com>
2019-01-10 18:45:00 +0900

    community list finish

--- DowaDog/Sources/AppDelegate.swift
+++ DowaDog/Sources/AppDelegate.swift
@@ -14,6 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+    // landscape lock
+    var enableAllOrientation = false
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if (enableAllOrientation == true){
+            return UIInterfaceOrientationMask.allButUpsideDown
+        }
+        return UIInterfaceOrientationMask.portrait
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
