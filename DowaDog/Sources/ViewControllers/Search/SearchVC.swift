d057a03e0c13cf97bb08595350a4f2323b143223
wookeon <design_pt@naver.com>
2019-01-12 01:12:01 +0900

    search VC constant move

--- DowaDog/Sources/ViewControllers/Search/SearchVC.swift
+++ DowaDog/Sources/ViewControllers/Search/SearchVC.swift
@@ -17,9 +17,10 @@ class SearchVC: UIViewController {
     var searchDogList = [EmergenDog]()
     
     var flag = true
-    var searchFlag = false
     var searchWord:String?
     var delegate: SendDataDelegate?
+    
+    var keyboardHeight: CGFloat = 0.0
     
     @IBOutlet var searchTF: UITextField!
     @IBOutlet var searchBtn: UIButton!
@@ -33,46 +34,68 @@ class SearchVC: UIViewController {
     @IBOutlet var keywordBtn8: keywordButton!
     @IBOutlet var keywordBtn9: keywordButton!
     
+    
+    
+    @IBOutlet var constraint: NSLayoutConstraint!
+    @IBOutlet var searchBtnConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var userName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        self.setNavigationBar()
-        //        self.setBackBtn(color: UIColor.init(red: 111/255, green: 112/255, blue: 111/255, alpha: 1.0))
+        
+        // init
+        self.setNavigationBarClear()
+
+        
         
         searchTF.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         
         searchTF.clearButtonMode = .whileEditing
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        MyInfoService.shared.getMyInfo() {
+            (data) in
+            
+            self.userName.text = "\(self.gsno(data.data?.name))님,"
+        }
+        
+        registerForKeyboardNotifications()
+    }
+
+    // 옵저버 해지
+    override func viewWillDisappear(_ animated: Bool) {
+        unregisterForKeyboardNotifications()
+    }
+    
     @objc func textFieldDidChange(textField: UITextField){
         
         searchWord = searchTF.text
         
-        //여기서 분기
-        EmergenDogService.shared.findAnimalList(type: "", region: "", remainNoticeDate: 300, searchWord: searchWord, page: 0, limit: 10){ [weak self]
+        EmergenDogService.shared.findAnimalList(type: "", region: "", remainNoticeDate: 300, searchWord: searchWord, page: 0, limit: 10){
             (data) in
-            guard self != nil else {return}
             
             print("---여기--------------")
             print(data)
             print("---여기--------------")
-            self?.searchDogList = data
-            if  self?.searchDogList.count == 0{
-                self?.flag  =  false
-            }else {
-                self?.flag = true
-            }
             
-        
+            if data.count == 0 {
+                self.flag  =  false
+            } else {
+                self.flag = true
+            }
         }
         
         if searchTF.text != "" {
-            searchBtn.backgroundColor = UIColor(red: 0/255, green: 218/255, blue: 218/255, alpha: 1.0)
-            searchFlag = true
+            searchBtn.backgroundColor = UIColor(red: 255/255, green: 194/255, blue: 51/255, alpha: 1.0)
         } else {
             searchBtn.backgroundColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1.0)
-            searchFlag = false
         }
     }
+    
     
     
     func findByHashtag(hashtag: String) {
@@ -84,11 +107,12 @@ class SearchVC: UIViewController {
         }
     }
     
-    @IBAction func searchBtnAction(_ sender: Any) {
+    @IBAction func searchBtnAction(_ sender: UIButton) {
 
          searchWord = searchTF.text
         
-        if searchFlag == true {
+        if sender.backgroundColor == UIColor(red: 255/255, green: 194/255, blue: 51/255, alpha: 1.0) {
+
             
             if let data = searchTF.text {
                 delegate?.sendData(data: data)
@@ -125,5 +149,68 @@ class SearchVC: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         self.view.endEditing(true)
+    }
+}
+
+extension SearchVC: UIGestureRecognizerDelegate {
+    func initGestureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTapTextView(_:)))
+        tap.delegate = self
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTapTextView(_ sender: UITapGestureRecognizer) {
+        self.searchTF.resignFirstResponder()
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if (touch.view?.isDescendant(of: searchTF))! {
+            
+            return false
+        }
+        
+        return true
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            keyboardHeight = keyboardRectangle.height
+        }
+        
+        
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            self.searchBtnConstraint.constant = self.keyboardHeight
+            self.constraint.constant = 16
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else {return}
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {return}
+        UIView.animate(withDuration: duration, delay: 0.0, options: .init(rawValue: curve), animations: {
+            self.searchBtnConstraint.constant = 0
+            self.constraint.constant = 46 //스택 뷰의 제약조건을 변경한다.
+            
+        })
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
